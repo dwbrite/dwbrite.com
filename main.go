9b865d02ae9f7a48e92557b906517e43d03bf430
dwbrite <dwbrite@gmail.com>
2018-10-10 01:17:26 -0400

    recreate dwbrite.com source for teacup 0.2.0 (partial functionality)

--- main.go
+++ main.go
@@ -4,72 +4,101 @@
  */
 
 package main
-/*
-import (
-	_ "github.com/lib/pq"
-	"regexp"
-					)
 
-const (
-	DB_INFO = "user=devin dbname=dwbrite_com sslmode=disable"
-	CERT = "certs/dwbrite.com.cert"
-	KEY = "certs/dwbrite.com.key"
-	PORT = ":41234"
-	POSTS_PER_PAGE = 5
+import (
+	. "./teacup"
+	_ "github.com/lib/pq"
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+	"regexp"
+	"time"
 )
 
-var fileWhitelist = regexp.MustCompile("^/.*\\.(html|css|js|png|jpg|gif|webm)$")
-var dirBlacklist = regexp.MustCompile("^/(certs|examples|tmpl)/?.*$")
-
-var rPost = regexp.MustCompile("^/blog/post/?.*?$")
-var rBlog = regexp.MustCompile("^/blog/?.*$")
-var rPortfolio = regexp.MustCompile("^/work/?.*$")
-
-/*
-type project struct {
-	Summary string
-	Content page
-}*/
+type blogPage struct {
+	BlogPosts	[]PageContents
+	Title		string
+	PageNum		uint32
+}
 
 func main() {
-	//http.HandleFunc("/", matchRequest)
-	//log.Fatal(http.ListenAndServeTLS(PORT, CERT, KEY, nil))
-}
-
-/*
-
-func checkError(w http.ResponseWriter, err error) bool {
-	if err != nil { http.Error(w, err.Error(), http.StatusInternalServerError) }
-	return err != nil
-}
-
-func matchRequest(writer http.ResponseWriter, request *http.Request) {
-	//TODO: "/blog/", "/blog/article/", "/work/"
-	path := request.URL.Path
-	switch {
-	case dirBlacklist.MatchString(path):
-		serveError(writer, http.StatusForbidden)
-	case path == "/":
-		servePage(writer, "About Me")
-	case rPost.MatchString(path):
-		servePost(writer, request)
-	case rBlog.MatchString(path):
-		serveBlog(writer, request)
-	case rPortfolio.MatchString(path):
-	default:
-		serveFile(writer, request)
+	file, err := os.Create("dwbrite.com.log")
+	if err != nil {
+		log.Fatal(err)
 	}
+
+	t := NewTeacup(
+		41234,
+		"user=devin dbname=dwbrite_com sslmode=disable",
+		&TlsKeyPair{
+			"certs/dwbrite.com.cert",
+			"certs/dwbrite.com.key",
+		},
+
+		*regexp.MustCompile("^/.*\\.(html|css|js|png|jpg|gif|webm|ico|md|mp3|mp4)$"),
+		*regexp.MustCompile("^/(certs|examples|tmpl)/?.*$"),
+
+		log.New(file, "", log.LstdFlags|log.Lshortfile),
+		)
+
+	t.CreateTable("posts", false)
+	t.CreateTable("projects", true)
+	//t.CreateTable("pages", true)
+
+	//"^...?.*?$"
+	t.AddDynamicPage("^/blog/?.*?$", "posts", template.FuncMap{"formatDate" : formatDate}, blogQuery, "tmpl/blogpost.gohtml", "tmpl/blogpage.gohtml", "tmpl/base.gohtml")
+
+	t.StartServer()
 }
 
-func serveError(writer http.ResponseWriter, httpStatus int) {
-	writer.WriteHeader(httpStatus)
-	servePage(writer, strconv.Itoa(httpStatus))
+func formatDate(t time.Time) string {
+	{ return t.Format("2006-01-02") }
 }
 
-func serveFile(writer http.ResponseWriter, request *http.Request) {
-	if fileWhitelist.MatchString(request.URL.Path) {
-		http.ServeFile(writer, request, request.URL.Path[1:])
+
+func blogQuery(request http.Request, dbInfo string) interface{} {
+	query := request.URL.Query()
+	postUid := query.Get("post")
+	pageNum := query.Get("page")
+
+	blogPosts := blogPage{
+		[]PageContents{},
+		"Blog",
+		0,
+	}
+
+	/*pageNum, err := strconv.Atoi(request.URL.Query().Get("page"))
+	if err != nil {
+		pageNum = -1
+	}*/
+	if postUid != "" {
+		post, _ := SelectContentByUid(postUid, "posts", dbInfo)
+		if post != nil {
+			blogPosts.BlogPosts = append(blogPosts.BlogPosts, *post)
+			return blogPosts
+		}
+	} /* TODO("Create Query for multiple ")
+	else if pageNum == -1 {
+		posts, err := t.selectPosts("SELECT * FROM " +
+			"(SELECT * FROM posts ORDER BY post_date DESC " +
+			"LIMIT 5) AS derivedTable " +
+			"ORDER BY post_date DESC;")
 	} else {
-		serveError(writer, http.StatusForbidden)
+		posts, err := t.selectPosts("SELECT * FROM posts ORDER BY post_date ASC " +
+			"LIMIT " + strconv.Itoa(t.PostsPerPage) +
+			"OFFSET " + strconv.Itoa(t.PostsPerPage*(pageNum)) + ";")
 	}
-}*
+
+	blogPosts.BlogPosts = posts
+	err = postsTemplate.ExecuteTemplate(writer, "base", blogPosts)
+	if t.checkError(writer, err) {
+		return
+	}*/
+
+	return nil //TODO(Return blog page 0)
+}
+
+func postPath(writer http.ResponseWriter) interface{} {
+	return writer
+}
