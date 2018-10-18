7e870914e6fed2814ecd0b7b4ddc51de6f30f92c
Devin Brite <devin@dwbrite.com>
2018-10-17 22:18:09 -0400

    Add cache fonts btn

--- add_fonts.js
+++ add_fonts.js
@@ -1,4 +1,18 @@
-window.onload = function() {
+dlFontsBtn = document.createElement('button');
+dlFontsBtn.id = "font-btn";
+dlFontsBtn.innerHTML = "Download Required Fonts";
+
+body = document.getElementsByTagName("body")[0];
+body.innerHTML = dlFontsBtn.outerHTML + body.innerHTML;
+
+dlFontsBtn = document.getElementById("font-btn");
+dlFontsBtn.onclick = downloadFonts;
+
+
+/*window.onload = downloadFontsAsEmbedded;
+document.fonts.ready.then(embedFonts);
+
+function downloadFontsAsEmbedded() {
     var FiraSansLight = new FontFace('Fira Sans Light Embedded', 'url("fonts/FiraSans-Light.ttf")');
     document.fonts.add(FiraSansLight);
     var FiraCode = new FontFace('Fira Code Embedded', 'url("fonts/FiraCode-Regular.ttf")');
@@ -7,9 +21,9 @@ window.onload = function() {
     document.fonts.add(FiraSansSemiBold);
     var Merriweather = new FontFace('Merriweather Embedded', 'url("fonts/Merriweather-Regular.ttf")');
     document.fonts.add(Merriweather);
-};
+}
 
-document.fonts.ready.then( function() {
+function embedFonts() {
     style = document.createElement('style');
     style.type = 'text/css';
     style.innerHTML = `
@@ -18,4 +32,19 @@ document.fonts.ready.then( function() {
         h3, h4, h5, h6       { font-family: "Merriweather", "Merriweather Embedded", serif; }
         div.post-header time { font-family: "Fira Code", "Fira Code Embedded", monospace; }`;
     document.getElementsByTagName('head')[0].appendChild(style);
-});
+}*/
+
+function downloadFonts() {
+
+
+    var FiraSansLight = new FontFace('Fira Sans Light', 'url("fonts/FiraSans-Light.ttf")');
+    var FiraCode = new FontFace('Fira Code', 'url("fonts/FiraCode-Regular.ttf")');
+    var FiraSansSemiBold = new FontFace('Fira Sans SemiBold', 'url("fonts/FiraSans-SemiBold.ttf")');
+    var Merriweather = new FontFace('Merriweather', 'url("fonts/Merriweather-Regular.ttf")');
+    document.fonts.add(FiraSansLight);
+    document.fonts.add(FiraCode);
+    document.fonts.add(FiraSansSemiBold);
+    document.fonts.add(Merriweather);
+
+
+}
