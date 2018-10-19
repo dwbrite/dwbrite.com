e651967802ce1937aad83d113efba94034a4d7f3
dwbrite <dwbrite@gmail.com>
2018-10-19 03:33:10 -0400

    Fiddle with styles

--- slow_renderer.js
+++ slow_renderer.js
@@ -5,5 +5,5 @@ var ios = /iphone|ipod|ipad/.test(userAgent),
     gecko = /gecko\//.test(userAgent);
 
 if( (!gecko) && (ios || android || mobile)) {
-    document.body.style.background = "linear-gradient(-45deg, rgba(24, 0, 128, .5), rgba(255, 128, 0, .6))";
+    document.body.id="mobile-webkit";
 }
